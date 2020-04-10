1c4447e7a7db7fa684f6576f01c69f3f97d0fcb7
Tomilin Dmitry <gunpenguin1339@gmail.com>
2020-04-10 19:27:29 +0300

    Create somethings from branch name

---
+++ junkies/Assets/Scripts/Entities/Products/Drugs/Cocaine.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cocaine : MonoBehaviour, IDrug
+{
+    public int sellPrice {get;} = 50;
+    public int buyPrice {get;} = 30;
+    public string productName {get;} = "Cocaine";
+    public string description {get;} = "High-end white drug";
+    
+}
---
+++ junkies/Assets/Scripts/Entities/Products/Goods/Candies.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candies : MonoBehaviour, IGoods
+{
+    [SerializeField]
+    public int buyPrice {get;} = 4;
+    [SerializeField]
+    public string productName {get;} = "Candy";
+    [SerializeField]
+    public string description {get;} = "Yummy blue candies";
+}
